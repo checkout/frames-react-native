b060a3a3b7b6ce3d86048f4984386129d885f0bf
edimas <dimasefthymios@gmail.com>
2024-02-23 19:30:22 +0200

    update field to have ref

--- src/components/CardNumber.tsx
+++ src/components/CardNumber.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { StyleSheet, View, TextInput, Image } from "react-native";
 
 import { BIN_CHANGE, CARD_CHANGE } from "../utils/actions";
@@ -6,75 +6,82 @@ import { FramesConsumer } from "../Frames";
 import { FramesCardFieldProps } from "../types/types";
 import { DEFAULT_CARD_NUMBER_PLACEHOLDER } from "../utils/constants";
 
-const CardNumber: React.FC<FramesCardFieldProps> = (props) => {
-  return (
-    <FramesConsumer>
-      {({ state, dispatch }) => {
-        if (state?.cardNumber === undefined) {
-          throw "It looks like you are trying to render the CardNumber outside of the Frames Component.";
-        }
-        return (
-          <View style={styles.wrapper}>
-            <TextInput
-              autoCompleteType="cc-number"
-              keyboardType="number-pad"
-              returnKeyType="done"
-              placeholder={DEFAULT_CARD_NUMBER_PLACEHOLDER}
-              {...props}
-              value={state.cardNumber}
-              style={[styles.cardNumber, props.style]}
-              onChangeText={(val: string) => {
-                dispatch({ type: CARD_CHANGE, payload: val });
-                if (
-                  val.replace(/[^0-9]/g, "").length >= 8 &&
-                  val.replace(/[^0-9]/g, "").substring(0, 8) !==
-                    state.cardBin.bin
-                ) {
-                  dispatch({
-                    type: BIN_CHANGE,
-                    payload: val.replace(/[^0-9]/g, "").substring(0, 8),
-                  });
+const CardNumber = forwardRef<TextInput, FramesCardFieldProps>((props, ref) => {
+    return (
+        <FramesConsumer>
+            {({ state, dispatch }) => {
+                if (state?.cardNumber === undefined) {
+                    throw "It looks like you are trying to render the CardNumber outside of the Frames Component.";
                 }
-              }}
-            />
-            {props.showIcon === undefined || props.showIcon ? (
-              <View style={styles.schemeIconContainer}>
-                {state.cardIcon ? (
-                  <Image style={styles.scheme} source={state.cardIcon} />
-                ) : undefined}
-              </View>
-            ) : undefined}
-          </View>
-        );
-      }}
-    </FramesConsumer>
-  );
-};
+                return (
+                    <View style={styles.wrapper}>
+                        <TextInput
+                            autoCompleteType="cc-number"
+                            keyboardType="number-pad"
+                            returnKeyType="done"
+                            placeholder={DEFAULT_CARD_NUMBER_PLACEHOLDER}
+                            {...props}
+                            value={state.cardNumber}
+                            ref={ref}
+                            style={[styles.cardNumber, props.style]}
+                            onChangeText={(val: string) => {
+                                dispatch({ type: CARD_CHANGE, payload: val });
+                                if (
+                                    val.replace(/[^0-9]/g, "").length >= 8 &&
+                                    val
+                                        .replace(/[^0-9]/g, "")
+                                        .substring(0, 8) !== state.cardBin.bin
+                                ) {
+                                    dispatch({
+                                        type: BIN_CHANGE,
+                                        payload: val
+                                            .replace(/[^0-9]/g, "")
+                                            .substring(0, 8),
+                                    });
+                                }
+                            }}
+                        />
+                        {props.showIcon === undefined || props.showIcon ? (
+                            <View style={styles.schemeIconContainer}>
+                                {state.cardIcon ? (
+                                    <Image
+                                        style={styles.scheme}
+                                        source={state.cardIcon}
+                                    />
+                                ) : undefined}
+                            </View>
+                        ) : undefined}
+                    </View>
+                );
+            }}
+        </FramesConsumer>
+    );
+});
 
 const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: "#00000000",
-  },
-  cardNumber: {
-    borderColor: "black",
-    borderWidth: 1,
-    minHeight: 30,
-    paddingLeft: 15,
-  },
-  schemeIconContainer: {
-    height: "100%",
-    right: 10,
-    width: 50,
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  scheme: {
-    height: "50%",
-    width: "100%",
-    resizeMode: "contain",
-    alignItems: "center",
-  },
+    wrapper: {
+        backgroundColor: "#00000000",
+    },
+    cardNumber: {
+        borderColor: "black",
+        borderWidth: 1,
+        minHeight: 30,
+        paddingLeft: 15,
+    },
+    schemeIconContainer: {
+        height: "100%",
+        right: 10,
+        width: 50,
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    scheme: {
+        height: "50%",
+        width: "100%",
+        resizeMode: "contain",
+        alignItems: "center",
+    },
 });
 
 export default CardNumber;
--- src/components/ExpiryDate.tsx
+++ src/components/ExpiryDate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { StyleSheet, TextInput } from "react-native";
 
 import { FramesConsumer } from "../Frames";
@@ -6,41 +6,42 @@ import { DEFAULT_CARD_EXPIRY_DATE_PLACEHOLDER } from "../utils/constants";
 import { DATE_CHANGE } from "../utils/actions";
 import { FramesFieldProps } from "../types/types";
 
-const ExpiryDate: React.SFC<FramesFieldProps> = (props) => {
-  return (
-    <FramesConsumer>
-      {({ state, dispatch }) => {
-        if (state?.expiryDate === undefined) {
-          throw "It looks like you are trying to render the ExpiryDate outside of the Frames Component.";
-        }
-        return (
-          <TextInput
-            autoCompleteType="cc-exp"
-            keyboardType="number-pad"
-            maxLength={5}
-            returnKeyType="done"
-            placeholder={DEFAULT_CARD_EXPIRY_DATE_PLACEHOLDER}
-            {...props}
-            style={[styles.expiryDate, props.style]}
-            value={state.expiryDate}
-            onChangeText={(val: string) =>
-              dispatch({ type: DATE_CHANGE, payload: val })
-            }
-          />
-        );
-      }}
-    </FramesConsumer>
-  );
-};
+const ExpiryDate = forwardRef<TextInput, FramesFieldProps>((props, ref) => {
+    return (
+        <FramesConsumer>
+            {({ state, dispatch }) => {
+                if (state?.expiryDate === undefined) {
+                    throw "It looks like you are trying to render the ExpiryDate outside of the Frames Component.";
+                }
+                return (
+                    <TextInput
+                        autoCompleteType="cc-exp"
+                        keyboardType="number-pad"
+                        maxLength={5}
+                        returnKeyType="done"
+                        placeholder={DEFAULT_CARD_EXPIRY_DATE_PLACEHOLDER}
+                        {...props}
+                        ref={ref}
+                        style={[styles.expiryDate, props.style]}
+                        value={state.expiryDate}
+                        onChangeText={(val: string) =>
+                            dispatch({ type: DATE_CHANGE, payload: val })
+                        }
+                    />
+                );
+            }}
+        </FramesConsumer>
+    );
+});
 
 const styles = StyleSheet.create({
-  expiryDate: {
-    paddingLeft: 15,
-    borderColor: "black",
-    borderWidth: 1,
-    justifyContent: "center",
-    minHeight: 30,
-  },
+    expiryDate: {
+        paddingLeft: 15,
+        borderColor: "black",
+        borderWidth: 1,
+        justifyContent: "center",
+        minHeight: 30,
+    },
 });
 
 export default ExpiryDate;
